1363ad63cd3ee834fa23b1c7b5d4ce6e3c334737
robotscott94 <78816334+robotscott94@users.noreply.github.com>
2023-02-22 16:49:20 -0500

    Add files via upload
    Added sql file and image of database design scheme

---
+++ emp_db_design.sql
@@ -0,0 +1,167 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Create tables using QuickDBD schema
+
+-- Create departments table
+CREATE TABLE departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+-- Create dept_emp table
+-- There are two foreign keys (composite key) because neither column has all unique values
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR   NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (
+        emp_no, dept_no
+     )
+);
+
+-- Add foreign key references to dept_emp table
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_employees_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_employees_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+-- Create dept_manager table
+CREATE TABLE dept_manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no INT   NOT NULL,
+    CONSTRAINT pk_dept_manager PRIMARY KEY (
+        emp_no
+     )
+);
+
+-- Add foreign key references to dept_manager table
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_managers_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
+
+ALTER TABLE dept_manager ADD CONSTRAINT fk_dept_managers_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+
+-- Create employees table
+CREATE TABLE employees (
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR   NOT NULL,
+    birth_date VARCHAR   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date VARCHAR   NOT NULL,
+    CONSTRAINT pk_employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+-- Convert date in string format to SQL 'Date' format
+ALTER TABLE employees ADD COLUMN formatted_hire_date DATE;
+UPDATE employees SET formatted_hire_date = to_date(to_char(to_date(my_date, 'MM/DD/YYYY'), 'YYYY-MM-DD'), 'YYYY-MM-DD');
+
+-- Add foreign key reference for employees
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+-- Create salaries table
+CREATE TABLE salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (
+        emp_no
+     )
+);
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+-- Create titles table
+CREATE TABLE titles (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (
+        title_id
+     )
+);
+
+-- Check to ensure all data is correctly uploaded
+SELECT * FROM departments
+
+
+--1
+--List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e.emp_no, e.first_name, e.last_name, e.sex, s.salary
+FROM employees as e
+INNER JOIN salaries as s
+ON e.emp_no = s.emp_no;
+
+
+--2
+--List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE RIGHT(hire_date,4)='1986';
+
+
+--3
+--List the manager of each department along with their department number, department name, 
+--employee number, last name, and first name.
+SELECT dm.emp_no, dm.dept_no, d.dept_name, e.last_name, e.first_name
+FROM dept_manager as dm
+INNER JOIN departments as d
+ON dm.dept_no = d.dept_no
+INNER JOIN employees as e
+ON dm.emp_no = e.emp_no;
+
+
+--4
+--List the department number for each employee along with that employee’s employee number, 
+--last name, first name, and department name.
+SELECT e.emp_no, de.dept_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp as de
+INNER JOIN employees as e
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON d.dept_no = de.dept_no;
+
+
+--5
+--List first name, last name, and sex of each employee whose first name is Hercules and 
+--whose last name begins with the letter B.
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+
+--6
+--List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+WHERE de.dept_no = 'd007';
+
+
+--7
+--List each employee in the Sales and Development departments, including their employee number, 
+--last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE de.dept_no = 'd007' OR de.dept_no = 'd005';
+
+--8
+--List the frequency counts, in descending order, of all the employee last names 
+--(that is, how many employees share each last name).
+SELECT last_name, COUNT(last_name) as "name_count"
+FROM employees
+GROUP BY last_name
+ORDER BY "name_count" DESC
+
+
